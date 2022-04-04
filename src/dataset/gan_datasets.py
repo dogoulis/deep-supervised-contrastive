c08fbff6de0250657b22f0b8dc604ca028ec1882
Pantelis Dogoulis <pantelisdog@gmail.com>
2022-04-04 14:52:35 +0300

    Modify dataset as pl format

--- src/dataset/gan_datasets.py
+++ src/dataset/gan_datasets.py
@@ -1,3 +1,4 @@
+from random import shuffle
 from torch.utils.data import Dataset, DataLoader
 import torch
 import cv2
@@ -5,6 +6,36 @@ import numpy as np
 import pandas as pd
 from albumentations.pytorch import ToTensorV2
 import os
+import pytorch_lightning as pl
+
+
+
+class GANDataset(pl.LightningDataModule):
+    def __init__(self, dataset_path, batch_size=32, num_workers=8, transformrs=None, root_dir=None, csv_names=None):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.transforms = transformrs
+        self.root_dir = root_dir
+        self.csv_names = csv_names # list of [train_csv, val_csv, test_csv]
+
+    def setup(self, stage=None):
+
+        if stage in (None, 'fit'):
+            self.train_dataset = dataset2(dataset_path=self.csv_names[0], root_dir=self.root_dir, transforms=self.transforms)
+            self.val_dataset = dataset2(dataset_path=self.csv_names[1], root_dir=self.root_dir, transforms=self.transforms)
+        if stage in (None, 'test'):
+            self.test_dataset = dataset2(dataset_path=self.csv_names[2], root_dir=self.root_dir, transforms=self.transforms)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
 
 class dataset2(Dataset):
